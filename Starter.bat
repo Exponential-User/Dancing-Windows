5ff8f0e548ce7cc3949a5e4c4f53d5de606fe1f3
Exponential-User <158381144+Exponential-User@users.noreply.github.com>
2025-12-25 23:59:27 -0700

    Updated `Starter.bat`

--- Starter.bat
+++ Starter.bat
@@ -1,10 +1,12 @@
 @echo off
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+where python >nul 2>nul || (echo "Python not found on PATH. Install Python or add it to PATH" & pause & exit /b 1)
 SET "MODULES=pygame numpy librosa PIL mutagen"
 
 echo required modules: %MODULES%
 echo.
-echo Do you want to check and install required Python modules?
-choice /C YN /N /M "Y | N: "
+choice /C YN /N /M "Check and install modules? [Y | N]: "
 if ERRORLEVEL 2 goto :start
 
 echo Checking for required Python modules...
@@ -14,32 +16,29 @@ FOR %%M IN (%MODULES%) DO (
 )
 
 echo All required modules are installed.
-echo.
-echo Starting...
 timeout /t 2 /nobreak >nul
-goto :start
-
-
-:CheckAndInstall
-set "MOD_NAME=%~1"
-python -c "import %MOD_NAME%" 2>nul
-
-if %ERRORLEVEL% NEQ 0 (
-    echo Module %MOD_NAME% is not installed. Attempting to install...
-    python -m pip install %MOD_NAME%
-
-    if %ERRORLEVEL% NEQ 0 (
-        echo Failed to install %MOD_NAME%.
-        exit /b 1
-    )
-
-    echo Successfully installed %MOD_NAME%.
-) else (
-    echo Module %MOD_NAME% is already installed.
-)
-goto :EOF
-
 
 :start
 cls
 python main.py
+exit /b
+
+:CheckAndInstall
+setlocal enabledelayedexpansion
+set "MOD_NAME=%~1"
+python -c "import !MOD_NAME!" 2>nul
+
+if !ERRORLEVEL! NEQ 0 (
+    echo Module !MOD_NAME! is not installed. Attempting to install...
+    if "!MOD_NAME!"=="PIL" set "MOD_NAME=Pillow"
+    python -m pip install !MOD_NAME!
+    if !ERRORLEVEL! NEQ 0 (
+        echo Failed to install !MOD_NAME!.
+        exit /b 1
+    )
+    echo Successfully installed !MOD_NAME!.
+) else (
+    echo Module !MOD_NAME! is already installed.
+)
+endlocal
+exit /b
